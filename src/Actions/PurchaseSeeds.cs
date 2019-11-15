be03101489cb0297e6b54f305e406151f48ca806
LanceP51 <appleguy05@live.com>
2019-11-15 13:56:52 -0500

    check PR

--- src/Actions/PurchaseSeeds.cs
+++ src/Actions/PurchaseSeeds.cs
@@ -10,7 +10,7 @@ namespace Trestlebridge.Actions {
             // print option of Plants to buy
             Console.WriteLine ("1. Sesame");
             Console.WriteLine ("2. Sunflower");
-            Console.WriteLine ("2. Wildflower");
+            Console.WriteLine ("3. Wildflower");
 
             Console.WriteLine ();
             Console.WriteLine ("What are you buying today?");
@@ -24,6 +24,12 @@ namespace Trestlebridge.Actions {
             {
                 case 1:
                     ChooseNaturalField.CollectInput(farm, new Sesame());
+                    break;
+                 case 2:
+                    ChooseNaturalField.CollectInput(farm, new Sunflower());
+                    break;
+                 case 3:
+                    ChooseNaturalField.CollectInput(farm, new Wildflower());
                     break;
                 default:
                     break;
