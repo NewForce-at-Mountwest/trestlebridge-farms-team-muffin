56c4e34d42794e6f799c783e1bbef45f6d06259f
Newforce44 <allancook@crownmkg.com>
2019-11-15 13:51:05 -0500

    functioning chicken house option with addition to purchase list

---
+++ src/Actions/ChooseChickenHouse.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using Trestlebridge.Interfaces;
+using Trestlebridge.Models;
+using Trestlebridge.Models.Animals;
+
+namespace Trestlebridge.Actions
+{
+    public class ChooseChickenHouse
+    {
+        public static void CollectInput(Farm farm, Chicken chicken)
+        // clear the terminal
+        {
+            Utils.Clear();
+            // print out all types of grazing fields
+            for (int i = 0; i < farm.Chickens.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. Chicken House");
+            }
+
+            Console.WriteLine();
+
+            // How can I output the type of animal chosen here?
+            Console.WriteLine($"Place the animal where?");
+
+            Console.Write("> ");
+            // read which grazing field option is selected
+            int choice = Int32.Parse(Console.ReadLine());
+
+            // add the animal to the field
+            farm.Chickens[choice].AddResource(chicken);
+
+            /*
+                Couldn't get this to work. Can you?
+                Stretch goal. Only if the app is fully functional.
+             */
+            // farm.PurchaseResource<IGrazing>(animal, choice);
+
+        }
+    }
+}
