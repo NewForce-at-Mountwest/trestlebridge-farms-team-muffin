72d86e626e916535da5ee974c1a8272fda007a7b
Daryl Adkins <daryl.adkins@gmail.com>
2019-11-18 13:25:05 -0500

    pre ticket 2 PR creation

--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -4,19 +4,23 @@ using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class PurchaseStock {
-        public static void CollectInput (Farm farm) {
+namespace Trestlebridge.Actions
+{
+    public class PurchaseStock
+    {
+        public static void CollectInput(Farm farm)
+        {
             // print option of animals to buy
-            Console.WriteLine ("1. Cow");
-            Console.WriteLine ("2. Ostrich");
+            Console.WriteLine("1. Cow");
+            Console.WriteLine("2. Ostrich");
+            Console.WriteLine("3. Duck");
 
-            Console.WriteLine ();
-            Console.WriteLine ("What are you buying today?");
+            Console.WriteLine();
+            Console.WriteLine("What are you buying today?");
 
-            Console.Write ("> ");
+            Console.Write("> ");
             // read the user selection
-            string choice = Console.ReadLine ();
+            string choice = Console.ReadLine();
 
             // conditional to print from user selection
             switch (Int32.Parse(choice))
@@ -25,6 +29,10 @@ namespace Trestlebridge.Actions {
                     ChooseGrazingField.CollectInput(farm, new Cow());
                     break;
                 default:
+                    break;
+
+                case 2:
+                    ChooseDuckHouse.CollectInput(farm, new Duck());
                     break;
             }
         }
--- src/Models/Facilities/DuckHouse.cs
+++ src/Models/Facilities/DuckHouse.cs
@@ -2,44 +2,48 @@ using System;
 using System.Text;
 using System.Collections.Generic;
 using Trestlebridge.Interfaces;
-
+using Trestlebridge.Models.Animals;
 
 namespace Trestlebridge.Models.Facilities {
-//double check on Interface
-    public class DuckHouse : IFacility<IGrazing>
-    {
-        private int _capacity = 20;
+    public class DuckHouse : IFacility<DuckHouse>{
+        private int _capacity = 12;
         private Guid _id = Guid.NewGuid();
-
-        private List<IGrazing> _animals = new List<IGrazing>();
-
+        private List<DuckHouse> _ducks = new List<DuckHouse>();
         public double Capacity {
             get {
                 return _capacity;
             }
         }
 
-        public void AddResource (IGrazing animal)
-        {
-
-         _animals.Add(animal);
-        }
-
-        public void AddResource (List<IGrazing> animals)
+        public void AddResource (DuckHouse duck)
         {
             // TODO: implement this...
-         _animals.AddRange(animals);
+           _ducks.Add(duck);
         }
+
+        public void AddResource (List<DuckHouse> duck)
+        {
+            // TODO: implement this...
+             _ducks.AddRange(duck);
+        }
+
+
+
 
         public override string ToString()
         {
             StringBuilder output = new StringBuilder();
             string shortId = $"{this._id.ToString().Substring(this._id.ToString().Length - 6)}";
 
-            output.Append($"Grazing field {shortId} has {this._animals.Count} animals\n");
-            this._animals.ForEach(a => output.Append($"   {a}\n"));
+            output.Append($"Grazing field {shortId} has {this._ducks.Count} animals\n");
+            this._ducks.ForEach(a => output.Append($"   {a}\n"));
 
             return output.ToString();
+        }
+
+        internal void AddResource(Duck duck)
+        {
+            throw new NotImplementedException();
         }
     }
 }
